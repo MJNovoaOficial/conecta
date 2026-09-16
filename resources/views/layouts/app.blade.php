398a5938556aa76ce9311b747b0d997721956ad0
Maria Jose Novoa <maria.novoa@dimak.cl>
2026-09-16 09:56:01 -0300

    agregamos el logo como favicon de viñeta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title', 'Conecta - Mesa de Ayuda')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="icon" type="image/png" href="{{ asset('images/dimaking-logo.png') }}">
     <style>
         * { box-sizing: border-box; }
 
